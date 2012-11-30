1b4cd9a27dafec3e9e3b9b3a11a5f8ad4c00ea28
Nestor Urquiza <nestor.urquiza@gmail.com>
2012-11-30 12:25:46 -0500

    rsync happens under sudo

--- run.sh
+++ run.sh
@@ -45,7 +45,8 @@ IFS=$'\n'
 lines=($(cat ../recipes/${host}.sh)) # array
 IFS=$old_IFS
 #rsync the whole recipes directory
-runremote $user $host $password "rsync -avz --delete -e \"ssh -i $HOME/.ssh/id_rsa\" ../recipes $user@$host:~/recipes/"
+runremote $user $host $password "chown -R $user:$user ~/recipes"
+rsyncFile ../recipes/
 for recipe in "${lines[@]}"
 do
   if [[ $recipe =~ ^[0-9a-zA-Z].* ]]
